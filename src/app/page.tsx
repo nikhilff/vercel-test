94ac833a2cb5c14f7158d0a64b87968150822741
nikhilff <nikhilff@gmail.com>
2023-12-10 02:07:54 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Hello starter templates for Next.js.
+            Hello World starter templates for Next.js.
           </p>
         </a>
 
